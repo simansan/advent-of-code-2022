db5a701ccc3e4adc3ab18128655553029cda2d09
simansan <simon.hoogveld@gosuperscript.com>
2022-12-01 11:13:35 +0100

    feat(day1): solution part 2

--- src/day-1/solutionDay1.ts
+++ src/day-1/solutionDay1.ts
@@ -7,4 +7,10 @@ fs.readFile('./day1Data.txt', (err, data) => {
     })
     const solution = Math.max(...totalCaloriesPerElf)
     console.log(solution)
-})
+
+    const sortedTotalCaloriesPerElf = totalCaloriesPerElf.sort((a, b) => a < b ? 1 : a > b ? -1 : 0)
+    const biggestCaloriesElfCarriers = sortedTotalCaloriesPerElf.slice(0, 3)
+    const solutionPartTwo = biggestCaloriesElfCarriers.reduce((acc, curr) => acc+ Number(curr), 0)
+    console.log(solutionPartTwo)
+
+    })
